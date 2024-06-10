3880d14ad5796ea79d4b2a661c3945204bb33dbb
d875167 <dave@lubon.be>
2024-06-10 13:32:51 +0200

    update vdot

--- Scripts/Set-SessionHostConfiguration.ps1
+++ Scripts/Set-SessionHostConfiguration.ps1
@@ -84,6 +84,9 @@ try {
         ##############################################################
         # https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool
 
+        # Don't run vdot on server os
+        if((Get-ComputerInfo).WindowsInstallationType -eq "Client"){
+
                 # Download VDOT
                 $URL = 'https://github.com/The-Virtual-Desktop-Team/Virtual-Desktop-Optimization-Tool/archive/refs/heads/main.zip'
                 $ZIP = 'VDOT.zip'
@@ -94,28 +97,59 @@ try {
 
                 # Fix to disable AppX Packages
                 # As of 2/8/22, all AppX Packages are enabled by default
+                # Define the list of AppxPackages to disable
+                $appxPackagesToDisable = @(
+                "Microsoft.549981C3F5F10"
+                "Microsoft.BingNews",
+                "Microsoft.BingWeather",
+                "Microsoft.GamingApp",
+                "Microsoft.Getstarted",
+                "Microsoft.MicrosoftSolitaireCollection",
+                "Microsoft.OutlookForWindows",
+                "Microsoft.People",
+                "Microsoft.PowerAutomateDesktop",
+                "Microsoft.SkypeApp",
+                "Microsoft.WinDbg.Fast",
+                "Microsoft.Windows.DevHome",
+                "microsoft.windowscommunicationsapps",
+                "Microsoft.WindowsFeedbackHub",
+                "Microsoft.WindowsMaps",
+                "Microsoft.WindowsTerminal",
+                "Microsoft.Xbox.TCUI",
+                "Microsoft.XboxGameOverlay",
+                "Microsoft.XboxGamingOverlay",
+                "Microsoft.XboxIdentityProvider",
+                "Microsoft.XboxSpeechToTextOverlay",
+                "Microsoft.YourPhone",
+                "Microsoft.ZuneMusic",
+                "Microsoft.ZuneVideo",
+                "MicrosoftWindows.Client.WebExperience",
+                "Microsoft.XboxApp",
+                "Microsoft.MixedReality.Portal",
+                "Microsoft.Wallet" 
+                )
+
                 $Files = (Get-ChildItem -Path .\VDOT\Virtual-Desktop-Optimization-Tool-main -File -Recurse -Filter "AppxPackages.json").FullName
+
                 foreach ($File in $Files) {
-                        $Content = Get-Content -Path $File
-                        $Settings = $Content | ConvertFrom-Json
-                        $NewSettings = @()
-                        foreach ($Setting in $Settings) {
-                                $NewSettings += [pscustomobject][ordered]@{
-                                        AppxPackage = $Setting.AppxPackage
-                                        VDIState    = 'Disabled'
-                                        URL         = $Setting.URL
-                                        Description = $Setting.Description
+                        $jsonContent = Get-Content -Path $File | ConvertFrom-Json
+                        foreach ($package in $jsonContent) {
+                                # Check if the current AppxPackage is in the list to disable
+                                if ($appxPackagesToDisable -contains $package.AppxPackage) {
+                                # Update the VDIState property to 'Disabled'
+                                $package.VDIState = "Disabled"
                                 }
                         }
 
-                        $JSON = $NewSettings | ConvertTo-Json
-                        $JSON | Out-File -FilePath $File -Force
-                }
-
+                        $jsonContent | ConvertTo-Json | Set-Content -Path $File
+                }      
                 # Run VDOT
                 & .\VDOT\Virtual-Desktop-Optimization-Tool-main\Windows_VDOT.ps1 -Optimizations 'All' -AdvancedOptimizations 'Edge', 'RemoveLegacyIE' -AcceptEULA
 
-                Write-Log -Message 'Optimized the operating system using VDOT' -Type 'INFO'  
+                Write-Log -Message 'Optimized the operating system using VDOT' -Type 'INFO'
+        } else {
+                Write-Log -Message 'Server Os detected skip VDOT' -Type 'INFO'
+        }  
 
         ##############################################################
         #  Add Recommended AVD Settings
