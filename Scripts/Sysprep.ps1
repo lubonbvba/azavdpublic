81931e2ceb5b11bc7a19d5dbfc7ce60e667d51bd
d875167 <63637908+d875167@users.noreply.github.com>
2024-06-22 09:33:45 +0200

    Update Sysprep.ps1 params

--- Scripts/Sysprep.ps1
+++ Scripts/Sysprep.ps1
@@ -1,2 +1,2 @@
 # Run sysprep needs to be run by custom script extension
-Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /quit /quiet' -Wait
+Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList '/generalize /oobe /shutdown /quiet' -Wait
