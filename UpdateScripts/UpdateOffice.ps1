e392ed8c82ebfca562b0190e226b8aadd39e1db0
Dave <63637908+d875167@users.noreply.github.com>
2026-06-24 04:33:27 +0200

    Update LatestBuild for SemiAnnual Channel

--- UpdateScripts/UpdateOffice.ps1
+++ UpdateScripts/UpdateOffice.ps1
@@ -42,7 +42,7 @@ $officeChannel = Get-OfficeUpdateChannel
 
 if ($officeChannel -eq "SemiAnnual") {
     Write-Output "SemiAnnual Channel builds are not listed anymore in API set manually"
-    $LatestBuild = "16.0.19127.20648"
+    $LatestBuild = "16.0.19127.20678"
 } else {
     $LatestBuild = $CloudVersionInfo | Where-Object { $_.channelId -eq $officeChannel } | Select-Object -ExpandProperty latestVersion
 }
